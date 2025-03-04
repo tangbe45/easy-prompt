29f77a160510a9423ca92f5b43347d972c2c491e
tangbe lucien <lucientangbe45@gmail.com>
2025-03-04 01:59:12 +0100

    Create components

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
     <section className="w-full flex-center flex-col">
-      <h1>Hello You</h1>
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text_center"> AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
     </section>
   );
 }
